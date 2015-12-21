f9034d527d035aa019523da741e3a91de60b0adf
Ron Waldon <jokeyrhyme@gmail.com>
2015-12-22 08:53:57 +1100

    basic home screen implementation with nav/router

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,26 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('app', ['ionic'])
+
+.config(function ($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider.state('home', {
+    url: '/',
+    templateUrl: 'components/home/tpl.html'
+  });
+
+  $stateProvider.state('inspections', {
+    url: '/inspections',
+    templateUrl: 'components/inspections/tpl.html'
+  });
+
+  $stateProvider.state('pending', {
+    url: '/pending',
+    templateUrl: 'components/pending/tpl.html'
+  });
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,4 +40,4 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
